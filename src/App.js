4588d163e57b045b65b1dd48406312ac94a3d751
Robert <paul_bialli@hotmail.com>
2020-11-07 19:07:01 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Paul Robert Godoy Bialli Junior
         </p>
         <a
           className="App-link"
